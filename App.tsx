12e3d472a8c2985e9609232410483145d39dcac5
Peter Klingelhofer <peterklingelhofer@gmail.com>
2022-07-04 19:18:34 -0500

    feat: initial dark mode responsiveness, prettier default

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import CustomStatusBar from "./client/components/CustomStatusBar";
 
 export default function App() {
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === "dark";
+  const themeTextStyle = isDark ? styles.darkThemeText : styles.lightThemeText;
+  const themeContainerStyle = isDark
+    ? styles.darkContainer
+    : styles.lightContainer;
+
   return (
-    <View style={styles.container}>
-      <Text>Resonate</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={[styles.container, themeContainerStyle]}>
+        <CustomStatusBar />
+        <Text style={themeTextStyle}>Resonate</Text>
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  lightContainer: {
+    backgroundColor: "#fff",
+  },
+  darkContainer: {
+    backgroundColor: "#181A1B",
+  },
+  lightThemeText: {
+    color: "#181A1B",
+  },
+  darkThemeText: {
+    color: "#fff",
   },
 });
