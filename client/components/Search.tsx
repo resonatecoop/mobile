4eb640197ddb8c0eb04c0747a080f820ccce6511
Peter Klingelhofer <peterklingelhofer@gmail.com>
2022-08-08 06:41:26 -0400

    feat: add eslint, remove prettier

--- client/components/Search.tsx
+++ client/components/Search.tsx
@@ -1,7 +1,8 @@
+import * as React from "react";
 import { Text } from "react-native-paper";
 
 const Search = () => {
-  return <Text>Search</Text>;
+    return <Text>Search</Text>;
 };
 
 export default Search;
