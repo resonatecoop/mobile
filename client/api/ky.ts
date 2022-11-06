7fa7799149c853b1e6c5447c6e4901ad3c63394c
Dominic Lee <dominictwlee@gmail.com>
2022-11-06 22:47:41 +0000

    feat: fetch updated artists from home

--- client/api/ky.ts
+++ client/api/ky.ts
@@ -3,6 +3,8 @@ import ky from "ky";
 
 const baseUrl = Constants?.manifest?.extra?.apiUrl as string;
 
+console.log(`${baseUrl}/api/v3`);
+
 const api = ky.create({ prefixUrl: `${baseUrl}/api/v3` });
 
 export default api;
--- client/components/Home/index.tsx
+++ client/components/Home/index.tsx
@@ -1,10 +1,25 @@
+import React from "react";
+import * as dotenv from "dotenv";
+import { View, FlatList } from "react-native";
 import { Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useLastUpdatedArtists } from "../../api";
 
 const Home = () => {
+  const { data: lastUpdatedArtists, error } = useLastUpdatedArtists();
+
+  console.log(error, "DATA");
+
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <View>
+        <Text>Featured Playlists</Text>
+        <FlatList
+          data={lastUpdatedArtists?.data}
+          horizontal
+          renderItem={({ item }) => <Text>{item.displayName}</Text>}
+        />
+      </View>
     </SafeAreaView>
   );
 };
