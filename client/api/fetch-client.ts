d91051dc56206464c3b5254ffedff688a4ac4173
Dominic Lee <dominictwlee@gmail.com>
2022-11-11 20:54:37 +0000

    feat: implement fetch client

---
+++ client/api/fetch-client.ts
@@ -0,0 +1,135 @@
+import { FetchError } from "./error";
+import fetchWithTimeout, { FetchOptions } from "./fetch-with-timeout";
+
+interface ServiceClientOptions {
+  host?: string;
+  headers?: Record<string, string>;
+  timeout?: number;
+}
+
+export class FetchClient {
+  options: ServiceClientOptions;
+  constructor(options: ServiceClientOptions) {
+    this.options = options;
+  }
+
+  mergeOptions(options?: ServiceClientOptions): ServiceClientOptions {
+    return {
+      ...this.options,
+      ...options,
+      headers: this.mergeHeaders(options?.headers),
+    };
+  }
+
+  mergeHeaders(newHeaders: Record<string, string> = {}) {
+    const headers = { ...this.options.headers };
+    for (const key of Object.keys(newHeaders)) {
+      const value = newHeaders[key];
+      if (value === undefined) {
+        delete headers[key];
+      } else {
+        headers[key] = value;
+      }
+    }
+    return headers;
+  }
+
+  setOptions(options?: ServiceClientOptions) {
+    this.options = this.mergeOptions(options);
+  }
+
+  extend(options: ServiceClientOptions) {
+    return new FetchClient(this.mergeOptions(options));
+  }
+
+  getURL(endpoint: string, prefix: string) {
+    const result = new URL(endpoint, prefix);
+    return result.toString();
+  }
+
+  request(
+    _method: string,
+    endpoint: string,
+    body?: BodyInit,
+    options?: ServiceClientOptions
+  ) {
+    const { host, headers, timeout = 0 } = this.mergeOptions(options);
+    const url = this.getURL(endpoint, host || "");
+    const method = _method.toUpperCase();
+    const opts: FetchOptions = {
+      method,
+      headers,
+      timeout,
+    };
+
+    // fetch will throw if given body for these
+    if (method !== "GET" && method !== "HEAD") {
+      opts.body = body;
+    }
+
+    return fetchWithTimeout(url, opts);
+  }
+
+  json<T, U = unknown>(
+    method: string,
+    endpoint: string,
+    payload: U | undefined,
+    options: Partial<ServiceClientOptions> = { headers: {} }
+  ) {
+    if (!options.headers) {
+      options.headers = {};
+    }
+
+    options.headers["Content-Type"] = "application/json";
+    options.headers["Accept"] = "application/json";
+
+    return this.request(
+      method,
+      endpoint,
+      JSON.stringify(payload),
+      options
+    ).then<T>((res) => (res.status === 204 ? (null as T) : parseJSON(res)));
+  }
+
+  post<T, U = unknown>(
+    endpoint: string,
+    payload?: U,
+    options?: ServiceClientOptions
+  ) {
+    return this.json<T>("POST", endpoint, payload, options);
+  }
+
+  patch<T, U = unknown>(
+    endpoint: string,
+    payload?: U,
+    options?: ServiceClientOptions
+  ) {
+    return this.json<T, U>("PATCH", endpoint, payload, options);
+  }
+
+  get<T, U extends Record<string, string>>(
+    endpoint: string,
+    params?: U,
+    options?: ServiceClientOptions
+  ) {
+    if (params) {
+      endpoint += "?" + new URLSearchParams(params).toString();
+    }
+    return this.json<T>("GET", endpoint, {}, options);
+  }
+
+  submit<T>(endpoint: string, body: BodyInit, options?: ServiceClientOptions) {
+    return this.request("POST", endpoint, body, {
+      ...options,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    }).then<T>(parseJSON);
+  }
+}
+
+function parseJSON(res: Response) {
+  return res.json().catch(() => {
+    return Promise.reject(
+      new FetchError("Failed to parse JSON payload", res.url, res)
+    );
+  });
+}
---
+++ client/api/fetch-with-timeout.ts
@@ -0,0 +1,45 @@
+import { FetchError } from "./error";
+
+export type FetchOptions = RequestInit & {
+  timeout?: number;
+};
+
+const DEFAULT_TIMEOUT = 15000;
+
+export default async function fetchWithTimeout(
+  url: string,
+  opts: FetchOptions
+): ReturnType<typeof fetch> {
+  const controller = new AbortController();
+
+  const headers = {
+    ...opts.headers,
+  };
+
+  const abortTimer = setTimeout(() => {
+    controller.abort("request timed out");
+  }, opts.timeout || DEFAULT_TIMEOUT);
+
+  return fetch(url, {
+    ...opts,
+    headers,
+    signal: controller.signal,
+  })
+    .then(async (res) => {
+      if (!res.ok) {
+        const errorMessage = await res.text();
+
+        const err = new FetchError(
+          `Request failed: ${url} ${res.status} ${errorMessage}`,
+          url,
+          res
+        );
+
+        return Promise.reject(err);
+      }
+      return res;
+    })
+    .finally(() => {
+      clearTimeout(abortTimer);
+    });
+}
