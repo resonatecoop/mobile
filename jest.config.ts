6bea14ba6c2b2a99fd1bdcd9cac0c287fd074ef1
Dominic Lee <dominictwlee@gmail.com>
2022-11-12 11:24:57 +0000

    chore: add default expo config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "@jest/types";
 
-const config: Config.InitialOptions = {};
+const config: Config.InitialOptions = {
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+};
 
 export default config;
