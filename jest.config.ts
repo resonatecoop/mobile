7457202bd90aed446051192f7ffdae05b0d99e13
Dominic Lee <dominictwlee@gmail.com>
2022-11-12 11:29:43 +0000

    chore: add testing library

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,7 @@ const config: Config.InitialOptions = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
 };
 
 export default config;
