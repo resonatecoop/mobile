f21db70ff7cebd6048476c45a791aa93781d69c6
Dominic Lee <dominictwlee@gmail.com>
2022-12-03 11:26:31 +0000

    feat: align search bar to header

--- client/components/Search/index.tsx
+++ client/components/Search/index.tsx
@@ -1,33 +1,19 @@
 import React from "react";
-import { View } from "react-native";
-import { Text, Searchbar, useTheme } from "react-native-paper";
+import { Searchbar, useTheme } from "react-native-paper";
 
-import { PLAYER_HEIGHT } from "../../constants";
-import { useKeyboardVisibility } from "../../context/keyboard";
 import ScreenView from "../common/ScreenView";
 
 const Search = () => {
-  const keyboardVisible = useKeyboardVisibility();
   const [searchQuery, setSearchQuery] = React.useState("");
   const theme = useTheme();
   return (
-    <ScreenView
-      style={{
-        height: "100%",
-        justifyContent: "space-between",
-      }}
-    >
-      <View>
-        <Text>SafeAreaView Search</Text>
-      </View>
+    <ScreenView>
       <Searchbar
-        blurOnSubmit
         placeholder="Search"
         value={searchQuery}
         onChangeText={setSearchQuery}
+        elevation={1}
         style={{
-          marginBottom: 4,
-          transform: [{ translateY: keyboardVisible ? 0 : -PLAYER_HEIGHT }],
           backgroundColor: theme.colors.surface,
         }}
       />
